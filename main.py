cae7a449f1d77dfa0027ec170b4ddc94b7080d34
faisalhelmi <faisal.helmi.w@gmail.com>
2021-10-17 20:48:20 +0700

    Auth Ver 5

--- main.py
+++ main.py
@@ -131,13 +131,9 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
 
 
 @app.get("/users/me/", response_model=User)
-async def read_users_me(current_user: User = Depends(get_current_active_user)):
+async def read_current_user(current_user: User = Depends(get_current_active_user)):
     return current_user
 
-
-@app.get("/users/me/items/")
-async def read_own_items(current_user: User = Depends(get_current_active_user)):
-    return [{"item_id": "Foo", "owner": current_user.username}]
 
 @app.get('/menu/{item_id}') 
 async def read_menu(item_id: int, current_user: User = Depends(get_current_active_user)): 
